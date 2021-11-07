4f810ac1d40c612734fd4c4cb7dd2340e6033333
lacakp <lacakp.contact@gmail.com>
2021-11-07 17:54:55 +0700

    Form Main Design

--- Fruit_Stock/Fruit_Stock/FrmMain.Designer.cs
+++ Fruit_Stock/Fruit_Stock/FrmMain.Designer.cs
@@ -31,6 +31,16 @@ namespace Fruit_Stock
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
             this.btnExit = new System.Windows.Forms.Button();
+            this.pbInvoice = new System.Windows.Forms.PictureBox();
+            this.pbProfile = new System.Windows.Forms.PictureBox();
+            this.pbStock = new System.Windows.Forms.PictureBox();
+            this.pbHistoryInvoice = new System.Windows.Forms.PictureBox();
+            this.pbHelp = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbInvoice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProfile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHistoryInvoice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHelp)).BeginInit();
             this.SuspendLayout();
             // 
             // btnExit
@@ -49,6 +59,60 @@ namespace Fruit_Stock
             this.btnExit.UseVisualStyleBackColor = false;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // pbInvoice
+            // 
+            this.pbInvoice.BackColor = System.Drawing.Color.Transparent;
+            this.pbInvoice.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbInvoice.BackgroundImage")));
+            this.pbInvoice.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbInvoice.Location = new System.Drawing.Point(229, 82);
+            this.pbInvoice.Name = "pbInvoice";
+            this.pbInvoice.Size = new System.Drawing.Size(251, 254);
+            this.pbInvoice.TabIndex = 6;
+            this.pbInvoice.TabStop = false;
+            // 
+            // pbProfile
+            // 
+            this.pbProfile.BackColor = System.Drawing.Color.Transparent;
+            this.pbProfile.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbProfile.BackgroundImage")));
+            this.pbProfile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbProfile.Location = new System.Drawing.Point(600, 82);
+            this.pbProfile.Name = "pbProfile";
+            this.pbProfile.Size = new System.Drawing.Size(251, 254);
+            this.pbProfile.TabIndex = 6;
+            this.pbProfile.TabStop = false;
+            // 
+            // pbStock
+            // 
+            this.pbStock.BackColor = System.Drawing.Color.Transparent;
+            this.pbStock.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbStock.BackgroundImage")));
+            this.pbStock.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbStock.Location = new System.Drawing.Point(229, 395);
+            this.pbStock.Name = "pbStock";
+            this.pbStock.Size = new System.Drawing.Size(251, 254);
+            this.pbStock.TabIndex = 6;
+            this.pbStock.TabStop = false;
+            // 
+            // pbHistoryInvoice
+            // 
+            this.pbHistoryInvoice.BackColor = System.Drawing.Color.Transparent;
+            this.pbHistoryInvoice.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbHistoryInvoice.BackgroundImage")));
+            this.pbHistoryInvoice.Location = new System.Drawing.Point(600, 395);
+            this.pbHistoryInvoice.Name = "pbHistoryInvoice";
+            this.pbHistoryInvoice.Size = new System.Drawing.Size(251, 254);
+            this.pbHistoryInvoice.TabIndex = 6;
+            this.pbHistoryInvoice.TabStop = false;
+            // 
+            // pbHelp
+            // 
+            this.pbHelp.BackColor = System.Drawing.Color.Transparent;
+            this.pbHelp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbHelp.BackgroundImage")));
+            this.pbHelp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbHelp.Location = new System.Drawing.Point(988, 264);
+            this.pbHelp.Name = "pbHelp";
+            this.pbHelp.Size = new System.Drawing.Size(171, 169);
+            this.pbHelp.TabIndex = 6;
+            this.pbHelp.TabStop = false;
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -56,6 +120,11 @@ namespace Fruit_Stock
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1348, 721);
+            this.Controls.Add(this.pbHistoryInvoice);
+            this.Controls.Add(this.pbHelp);
+            this.Controls.Add(this.pbProfile);
+            this.Controls.Add(this.pbStock);
+            this.Controls.Add(this.pbInvoice);
             this.Controls.Add(this.btnExit);
             this.DoubleBuffered = true;
             this.MaximumSize = new System.Drawing.Size(1980, 1080);
@@ -64,6 +133,11 @@ namespace Fruit_Stock
             this.Text = "Fruit Stock";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmMain_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pbInvoice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProfile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHistoryInvoice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHelp)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -71,6 +145,11 @@ namespace Fruit_Stock
         #endregion
 
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox pbInvoice;
+        private System.Windows.Forms.PictureBox pbProfile;
+        private System.Windows.Forms.PictureBox pbStock;
+        private System.Windows.Forms.PictureBox pbHistoryInvoice;
+        private System.Windows.Forms.PictureBox pbHelp;
     }
 }
 
