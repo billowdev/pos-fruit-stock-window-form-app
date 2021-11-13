f883518fa340473a5bc0e10ca40cf98397c7f3f7
lacakp <lacakp.contact@gmail.com>
2021-11-13 16:24:23 +0700

    FrmProfile Data Grid View

--- Fruit_Stock/Fruit_Stock/FrmProfile.cs
+++ Fruit_Stock/Fruit_Stock/FrmProfile.cs
@@ -22,26 +22,33 @@ namespace Fruit_Stock
 
         private void FrmProfile_Load(object sender, EventArgs e)
         {
-
-            AC.openConnection();
+            AC.closeConnection();
 
             if (AC.currentStatus == "user")
             {
+                showAllDataUser();
+
+                dgvAllMember.Hide();
                 btnEdit.Enabled = false;
                 btnNew.Enabled = false;
                 btnSave.Enabled = false;
                 btnCancel.Enabled = false;
+
             }
+            // -------------------------------------------- Admin --------------------------------------- //
             else
             {
                 showAllMember();
-
-
-           
             }
+        }
 
+        private void showAllDataUser()
+        {
+            
 
         }
+
+
         private void showAllMember() {
             string sqlMember = "select * from tb_login";
             if (AC.IsFind == true)
@@ -88,17 +95,74 @@ namespace Fruit_Stock
             }
             try
             {
-
+                // it is fill all data from tb_login to dataGridView
                 txtUsername.Text = dgvAllMember.Rows[e.RowIndex].Cells[1].Value.ToString();
                 txtPassword.Text = dgvAllMember.Rows[e.RowIndex].Cells[2].Value.ToString();
                 txtStatus.Text = dgvAllMember.Rows[e.RowIndex].Cells[3].Value.ToString();
+                txtEMPID.Text = dgvAllMember.Rows[e.RowIndex].Cells[4].Value.ToString();
 
+                
+                AC.sql = "SELECT * FROM tb_employee WHERE emp_id = @empid";
+                AC.cmd.Parameters.Clear();
+                AC.cmd.CommandType = CommandType.Text;
+                AC.cmd.CommandText = AC.sql;
+
+                AC.cmd.Parameters.AddWithValue("@empid", this.txtEMPID.Text.Trim().ToString());
+                AC.openConnection();
+
+
+                AC.rd = AC.cmd.ExecuteReader();
+
+                if (AC.rd.HasRows)
+                {
+                    while (AC.rd.Read())
+                    {
+                        txtName.Text = AC.rd[1].ToString();
+                        txtLastName.Text = AC.rd[2].ToString();
+
+                        if (AC.rd[3].ToString() == "ชาย")
+                        {
+                            rdbMale.Checked = true;
+                        }
+                        else
+                        {
+                            rdbFemale.Checked = true;
+                        }
+
+                        dtpBirthDate.Value = Convert.ToDateTime(AC.rd[4]);
+                        txtPhone.Text = AC.rd[5].ToString();
+
+                    }
+                }
+
+            AC.rd.Close();
+            AC.closeConnection();
             }
+
             catch
             {
                 MessageBox.Show("Error", "Error krub",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void FormatDataMember()
+        {
+            DataGridViewCellStyle cs = new DataGridViewCellStyle();
+            cs.Font = new Font("Ms Sans Serif", 10, FontStyle.Regular);
+            //dgvAllStduent.ColumnHeadersDefaultCellStyle = cs;
+            //dgvAllStduent.Columns[0].HeaderText = "รหัสนักศึกษา";
+            //dgvAllStduent.Columns[1].HeaderText = "ชื่อ";
+            //dgvAllStduent.Columns[2].HeaderText = "นามสกุล";
+            //dgvAllStduent.Columns[3].HeaderText = "วันเดือนปีเกิด";
+            //dgvAllStduent.Columns[4].HeaderText = "น้ำหนัก";
+
+            //dgvAllStduent.Columns[0].Width = 120;
+            //dgvAllStduent.Columns[1].Width = 140;
+            //dgvAllStduent.Columns[2].Width = 140;
+            //dgvAllStduent.Columns[3].Width = 140;
+            //dgvAllStduent.Columns[4].Width = 70;
+
         }
     }
 }
