ed22078a106ed5c7d40128a8e973121183284bfd
Akkarapon P <lacakkarapon@gmail.com>
2021-11-16 15:05:09 +0700

    Revert "Dev design"

---
+++ Fruit_Stock/Fruit_Stock/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fruit_Stock
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmLogin());
+            //Application.Run(new FrmProfile());
+            //Application.Run(new FrmInvoice());
+            //Application.Run(new FrmHistoryInvoice());
+            //Application.Run(new FrmStock());
+            //Application.Run(new FrmHelp());
+            //Application.Run(new FrmMain());
+        }
+    }
+}
